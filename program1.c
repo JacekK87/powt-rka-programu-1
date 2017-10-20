e72e85a050178c417b798689de8ca1e470e4304d
JacekK87 <jacekkielar1@wp.pl>
2017-10-20 09:42:13 +0200

    z_gitignore

--- program1.c
+++ program1.c
@@ -1,9 +1,17 @@
-#include<stdio.h>
- int main ()
+#include<stdio.h>/* dolaczamy biblioteki standardowej input-output*/
+ int main () /*funkcja glowna programu*/
 {
 
-	puts("\t Pierwszy\n program");
-	return 0;
+	puts("Pierwszy program");/*puts =>put string*/
+				/*wyswietla ciag zznakow*/
+				/*puts - funkcja z biblioteki standardowej*/
+				/*automatycznie przechodzi do nowej lini*/
+	putchar('g');
+	putchar('\n'); /*znak konca lini*/
+	putchar('\t');/*tabulator*/	
+/*	putchar('\r');*//*powrót karetki - kursor powraca na poczatek lini*/
+	putchar('\\');/*wypisuje \*/
 
+return 0;
 
-}
+}	/*klamry oznaczaja blok programu, zwykle cialo funkcji*/
