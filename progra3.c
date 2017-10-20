3bde195d013046ea0430ca5fe8fdc2001d748591
JacekK87 <jacekkielar1@wp.pl>
2017-10-20 11:52:29 +0200

    z konsoli

---
+++ progra3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int mnoz(int a, int b)		{ return a * b; }
+int dodaj (int a , int b)	{ return a + b; }
+int main(void)
+{
+	int a = 2, b = 5; /* delklaracja wraz z inizjalizacją*/
+	char wybor = '*';
+
+	scanf("%c" , &wybor); /*czytaj z konsloi z zapisz wartosc pod  wybor*/
+
+	/* if (warunek) ( blok jeśli  wyrazenie prawdziwe)
+	 else ( blok jeżeli wyrażenie fałszywe); */
+	
+	if(wybor == '*'){ /*sprawdza czy rowne to nie jest '=' */
+			printf("Wynik mnożenia  %i\n", mnoz (a, b));
+		}else{
+			printf("wynik dodowania  %i\n", dodaj(a, b));
+		}; /* ten srednik jests opcjonalny jezli wystepuje klamra*/
+
+	return 0;
+}
